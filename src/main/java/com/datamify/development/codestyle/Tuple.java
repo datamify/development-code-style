845460b256f7e1c2761b2432bf3e104788eaa585
Oleksii Dushenin <alexey.dushenin@gmail.com>
2021-07-24 23:16:09 +0300

    Java CheckStyle

---
+++ src/main/java/com/datamify/development/codestyle/Tuple.java
@@ -0,0 +1,13 @@
+package com.datamify.development.codestyle;
+
+/**
+ * Useful Javadoc.
+ */
+public class Tuple {
+
+  public void doSomething(int
+                              param) {
+    System.out.println("Param: " + param);
+  }
+
+}
